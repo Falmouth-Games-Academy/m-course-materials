35bffe7fbd59f794feb3cb80f773a8928d1118cd
Ed Powley <edpowley@gmail.com>
2019-11-12 01:39:07 +0000

    COMP702 lecture 8

--- COMP702/07/pathfinding_example_app/main.py
+++ COMP702/07/pathfinding_example_app/main.py
@@ -193,7 +193,7 @@ def main():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
                 break
 
-        do_string_pull = False
+        do_string_pull = True
         if path is not None and do_string_pull:
             string_pull(the_map, screen, path)
 
